ca877712836e4a3df535aae765ac2a414d3e0258
Torbjørn Vatn <torbjorn.vatn@unacast.com>
2020-01-24 11:05:10 +0100

    Add support for GCP_SA_KEY as an alternative to FIREBASE_TOKEN

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,25 @@
 
 set -e
 
-if [ -z "$FIREBASE_TOKEN" ]; then
-    echo "FIREBASE_TOKEN is required to run commands with the firebase cli"
-    exit 126
+if [ -z "$FIREBASE_TOKEN" ] && [ -z "$GCP_SA_KEY" ]; then
+  echo "Either FIREBASE_TOKEN or GCP_SA_KEY is required to run commands with the firebase cli"
+  exit 126
+fi
+
+if [ -n "$GCP_SA_KEY" ]; then
+  echo "Storing GCP_SA_KEY in /opt/gcp_key.json"
+  echo "$GCP_SA_KEY" | base64 -d > /opt/gcp_key.json
+  echo "Exporting GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json"
+  export GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json
 fi
 
 if [ -n "$PROJECT_PATH" ]; then
-    cd $PROJECT_PATH
+  cd "$PROJECT_PATH"
 fi
 
 if [ -n "$PROJECT_ID" ]; then
     echo "setting firebase project to $PROJECT_ID"
-    firebase use --add $PROJECT_ID
+    firebase use --add "$PROJECT_ID"
 fi
 
 sh -c "firebase $*"
