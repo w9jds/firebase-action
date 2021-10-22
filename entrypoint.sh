334d2512704c6e044f822b711431897dbc9a0113
Wei <weilinzung@gmail.com>
2021-10-22 09:33:06 -0400

    Revert "feat: export runtime config > .runtimeconfig.json"
    This reverts commit 31b7235ea7ef6d1e8b9c2216066a72121f54df43.

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,9 +23,4 @@ if [ -n "$PROJECT_ID" ]; then
     firebase use --add "$PROJECT_ID"
 fi
 
-if [ -n "$EXPORT_RUNTIME_CONFIG" = true ] ; then
-  echo "Exporting functions configuration variables > .runtimeconfig.json"
-  firebase functions:config:get > .runtimeconfig.json
-fi
-
 sh -c "firebase $*"
